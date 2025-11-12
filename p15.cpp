4a3364273cb0b94a1279b8f6be2e0fa2f6ed26a8
we1k0m <144484531+we1k0m@users.noreply.github.com>
2025-11-12 11:37:37 +0300

    Update p15.cpp

--- p15.cpp
+++ p15.cpp
@@ -1,152 +1,136 @@
 #include <iostream>
-#include <string>
 using namespace std;
-void clearScreen() {
-#ifdef _WIN32
-    system("cls");
-#else
-    system("clear");
-#endif
-}
-void drawLine(int size, char symbol) {
-    for (int i = 0; i < size; i++) {
-        cout << symbol << " ";
-    }
-    cout << endl;
-}
-void drawSquare(int size, char symbol, bool filled) {
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            if (filled || i == 0 || i == size - 1 || j == 0 || j == size - 1) {
-                cout << symbol << " ";
-            }
-            else {
-                cout << ". ";
-            }
-        }
-        cout << endl;
-    }
-}
-void drawRectangle(int height, int width, char symbol, bool filled) {
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            if (filled || i == 0 || i == height - 1 || j == 0 || j == width - 1) {
-                cout << symbol << " ";
-            }
-            else {
-                cout << ". ";
-            }
-        }
-        cout << endl;
-    }
-}
-
-void drawTriangle(int height, char symbol, bool filled) {
-    int width = 2 * height - 1;
-
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            int center = height - 1;
-            int left = center - i;
-            int right = center + i;
-
-            if (j == left || j == right || i == height - 1) {
-                cout << symbol << " ";
-            }
-            else if (filled && j > left && j < right) {
-                cout << symbol << " ";
-            }
-            else {
-                cout << ". ";
-            }
-        }
-        cout << endl;
-    }
-}
 
 int main() {
     setlocale(0, "");
-    int choice;
-    int size, width, height;
+    int choice, type, size;
     char symbol;
-    int typeChoice;
 
-    cout << "[ - ] Программа \"Геометрические фигуры\"" << endl << endl;
-    cout << "[ 1 ] Линия" << endl;
-    cout << "[ 2 ] Квадрат" << endl;
-    cout << "[ 3 ] Прямоугольник" << endl;
-    cout << "[ 4 ] Треугольник" << endl << endl;
-
+    cout << "[ - ] Программа \"Геометрические фигуры\"\n\n";
+    cout << "[ 1 ] Линия\n";
+    cout << "[ 2 ] Квадрат\n";
+    cout << "[ 3 ] Прямоугольник\n";
+    cout << "[ 4 ] Треугольник\n\n";
     cout << "[ - ] Выберите фигуру: ";
     cin >> choice;
 
-    clearScreen();
+    cout << "\n\n";
 
-    switch (choice) {
-    case 1:
-        cout << "[ - ] Фигура: \"Линия\"" << endl << endl;
+    if (choice == 4) {
+        cout << "[ - ] Фигура: \"Треугольник\"\n\n";
+        cout << "[ 1 ] Заполненный\n";
+        cout << "[ 2 ] Пустой\n\n";
+        cout << "[ - ] Выберите тип: ";
+        cin >> type;
+
+        cout << "\nДалее программа запрашивает высоту и текстуру(символ):\n\n";
         cout << "[ - ] Размер: ";
         cin >> size;
         cout << "[ - ] Текстура: ";
         cin >> symbol;
-        cout << endl << "Результат:" << endl << endl;
-        cout << "[ - ] Результат:" << endl << endl;
-        drawLine(size, symbol);
-        break;
 
-    case 2:
-        cout << "[ - ] Фигура: \"Квадрат\"" << endl << endl;
-        cout << "[ 1 ] Заполненный" << endl;
-        cout << "[ 2 ] Пустой" << endl << endl;
-        cout << "[ - ] Выберите тип: ";
-        cin >> typeChoice;
-        cout << endl << "Высота и текстура:" << endl << endl;
+        cout << "\nПрограмма выводит результат относительно введенных пользователем данных:\n\n";
+        cout << "[ - ] Результат:\n\n";
+
+        if (type == 1) {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size - i - 1; j++) {
+                    cout << ". ";
+                }
+                for (int j = 0; j < 2 * i + 1; j++) {
+                    cout << symbol << " ";
+                }
+                cout << "\n";
+            }
+        }
+        else {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size - i - 1; j++) {
+                    cout << ". ";
+                }
+                for (int j = 0; j < 2 * i + 1; j++) {
+                    if (j == 0 || j == 2 * i || i == size - 1) {
+                        cout << symbol << " ";
+                    }
+                    else {
+                        cout << ". ";
+                    }
+                }
+                cout << "\n";
+            }
+        }
+    }
+    else if (choice == 1) {
+        cout << "[ - ] Фигура: \"Линия\"\n\n";
         cout << "[ - ] Размер: ";
         cin >> size;
         cout << "[ - ] Текстура: ";
         cin >> symbol;
-        cout << endl << "Результат:" << endl << endl;
-        cout << "[ - ] Результат:" << endl << endl;
-        drawSquare(size, symbol, typeChoice == 1);
-        break;
 
-    case 3:
-        cout << "[ - ] Фигура: \"Прямоугольник\"" << endl << endl;
-        cout << "[ 1 ] Заполненный" << endl;
-        cout << "[ 2 ] Пустой" << endl << endl;
+        cout << "\nПрограмма выводит результат:\n\n";
+        cout << "[ - ] Результат:\n\n";
+
+        for (int i = 0; i < size; i++) {
+            cout << symbol << " ";
+        }
+        cout << "\n";
+    }
+    else if (choice == 2) {
+        cout << "[ - ] Фигура: \"Квадрат\"\n\n";
+        cout << "[ 1 ] Заполненный\n";
+        cout << "[ 2 ] Пустой\n\n";
         cout << "[ - ] Выберите тип: ";
-        cin >> typeChoice;
-        cout << endl << "Высота и текстура:" << endl << endl;
-        cout << "[ - ] Высота: ";
+        cin >> type;
+
+        cout << "\n[ - ] Размер: ";
+        cin >> size;
+        cout << "[ - ] Текстура: ";
+        cin >> symbol;
+
+        cout << "\nПрограмма выводит результат:\n\n";
+        cout << "[ - ] Результат:\n\n";
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (type == 1 || i == 0 || i == size - 1 || j == 0 || j == size - 1) {
+                    cout << symbol << " ";
+                }
+                else {
+                    cout << ". ";
+                }
+            }
+            cout << "\n";
+        }
+    }
+    else if (choice == 3) {
+        int width, height;
+        cout << "[ - ] Фигура: \"Прямоугольник\"\n\n";
+        cout << "[ 1 ] Заполненный\n";
+        cout << "[ 2 ] Пустой\n\n";
+        cout << "[ - ] Выберите тип: ";
+        cin >> type;
+
+        cout << "\n[ - ] Высота: ";
         cin >> height;
         cout << "[ - ] Ширина: ";
         cin >> width;
         cout << "[ - ] Текстура: ";
         cin >> symbol;
-        cout << endl << "Результат:" << endl << endl;
-        cout << "[ - ] Результат:" << endl << endl;
-        drawRectangle(height, width, symbol, typeChoice == 1);
-        break;
 
-    case 4:
-        cout << "[ - ] Фигура: \"Треугольник\"" << endl << endl;
-        cout << "[ 1 ] Заполненный" << endl;
-        cout << "[ 2 ] Пустой" << endl << endl;
-        cout << "[ - ] Выберите тип: ";
-        cin >> typeChoice;
-        cout << endl << "Высота и текстура:" << endl << endl;
-        cout << "[ - ] Размер: ";
-        cin >> size;
-        cout << "[ - ] Текстура: ";
-        cin >> symbol;
-        cout << endl << "Результат:" << endl << endl;
-        cout << "[ - ] Результат:" << endl << endl;
-        drawTriangle(size, symbol, typeChoice == 1);
-        break;
+        cout << "\nПрограмма выводит результат:\n\n";
+        cout << "[ - ] Результат:\n\n";
 
-    default:
-        cout << "Неверный выбор!" << endl;
-        break;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (type == 1 || i == 0 || i == height - 1 || j == 0 || j == width - 1) {
+                    cout << symbol << " ";
+                }
+                else {
+                    cout << ". ";
+                }
+            }
+            cout << "\n";
+        }
     }
 
     return 0;
