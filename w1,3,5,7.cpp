38a7241ac31551c8faa807d5dc72b3744b25ac93
we1k0m <144484531+we1k0m@users.noreply.github.com>
2025-12-02 12:15:36 +0300

    Create w1,3,5,7.cpp

---
+++ w1,3,5,7.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+void main() {
+   /* setlocale(0, "");
+    int x = 1;
+    int b = 0;
+    while (x != 0) {
+        cout << "Введите число: ";
+        cin >> x;
+        b += x;
+    }
+    cout << b;*/
+    setlocale(0, "");
+    /*int count = 0;
+    int sum = 0;
+    int x = 1;
+    while (x != 0) {
+        cout << "Введите число: ";
+        cin >> x;
+        sum += x;
+        if (x != 0) {
+            count += 1;
+        }
+    }
+    cout << "Количество чисел: " << count << endl << "Сумма: " << sum;*/
+    /*int x;
+    int count = 1;
+    cout << "Введите число: ";
+    cin >> x;
+    while (count != 11) {
+        cout << x << " x " << count << " = " << x * count << endl;
+        count += 1;
+    }*/
+    /*string password = "12345";
+    cout << "Введите пароль: ";
+    string x;
+    cin >> x;
+    while (x != password) {
+        cout << "Неверный пароль!" << endl;
+        cout << "Введите пароль: ";
+        cin >> x;
+    }
+    cout << "Добро пожаловать!";*/
+
+}
